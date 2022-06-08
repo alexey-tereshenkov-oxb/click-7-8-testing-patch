89317762496b06588953e77b3a0911f937b964e5
Alexey Tereshenkov <alexey.tereshenkov@oxbotica.com>
2022-06-08 11:14:31 +0100

    Simplify run command

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 docker build --build-arg CLICK_VERSION="7.1.2" -t click7/testing .
 docker build --build-arg CLICK_VERSION="8.1.3" -t click8/testing .
 
-docker run --rm -ti click7/testing /bin/bash -c "cd code; python3 -c 'import click; print(click.__version__)'; pytest -v"
-docker run --rm -ti click8/testing /bin/bash -c "cd code; python3 -c 'import click; print(click.__version__)'; pytest -v"
+COMMAND="cd code; python3 -c 'import click; print(click.__version__)'; pytest -v"
+docker run --rm -ti click7/testing /bin/bash -c "${COMMAND}"
+docker run --rm -ti click8/testing /bin/bash -c "${COMMAND}"
